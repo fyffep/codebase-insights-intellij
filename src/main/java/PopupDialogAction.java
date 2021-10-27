201408af066ef1f6b2515c473ddfa9aae822698c
Behar, Ethan T <ebehar@iu.edu>
2021-10-27 12:12:50 -0400

    committing proper project files

---
+++ src/main/java/PopupDialogAction.java
@@ -0,0 +1,31 @@
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.actionSystem.CommonDataKeys;
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.ui.Messages;
+import com.intellij.pom.Navigatable;
+import org.jetbrains.annotations.NotNull;
+
+public class PopupDialogAction extends AnAction {
+
+    @Override
+    public void update(AnActionEvent event) {
+        // Set the availability based on whether a project is open
+        Project aProject = event.getProject();
+        event.getPresentation().setEnabledAndVisible(aProject != null);
+    }
+
+    @Override
+    public void actionPerformed(@NotNull AnActionEvent event) {
+        // Using the event, create and show a dialog
+        Project currentProject = event.getProject();
+        StringBuffer dialogMessage = new StringBuffer(event.getPresentation().getText() + " Selected!");
+        String dialogTitle = event.getPresentation().getDescription();
+        // If an element is selected in the editor, add info about it.
+        Navigatable nav = event.getData(CommonDataKeys.NAVIGATABLE);
+        if(nav != null) {
+            dialogMessage.append(String.format("\nSelected Element: %s", nav.toString()));
+        }
+        Messages.showMessageDialog(currentProject, dialogMessage.toString(), dialogTitle, Messages.getInformationIcon());
+    }
+}
