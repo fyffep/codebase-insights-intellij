dc388517dd9bc80cf5f531182654347ef4170859
Behar, Ethan T <ebehar@iu.edu>
2021-11-04 16:44:43 -0400

    extracting child view creation into appropriate parents - getting commmit history to have banner (needs work to be more dynamic)

--- src/main/java/intellij_extension/views/CommitInfoSplitPane.java
+++ src/main/java/intellij_extension/views/CommitInfoSplitPane.java
@@ -4,28 +4,49 @@ import javafx.geometry.Orientation;
 import javafx.scene.control.SplitPane;
 import javafx.scene.layout.Background;
 import javafx.scene.layout.BackgroundFill;
+import javafx.scene.layout.Priority;
+import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 import javafx.scene.text.Text;
 
-public class CommitInfoSplitPane extends SplitPane {
+public class CommitInfoSplitPane extends SplitPane
+{
+    // Top half is Commit History for a single branch
+    private CommHistoryPane commitHistoryPane;
+
+    // Bottom half is Commit Details for a single commit in the selected branch
+    private CommitDetailsPane commitDetailsPane;
 
     public CommitInfoSplitPane() {
         super();
 
         setOrientation(Orientation.VERTICAL);
 
-        Text text = new Text();
-        text.setX(40);
-        text.setY(100);
-        text.setFont(new Font(25));
-        text.setText("ExtrasViews");
-        getChildren().add(text);
+//        Text text = new Text();
+//        text.setX(40);
+//        text.setY(100);
+//        text.setFont(new Font(25));
+//        text.setText("ExtrasViews");
+//        getChildren().add(text);
 
         // Fill background with default
         Color backgroundColor = Color.ORANGERED;
         BackgroundFill backgroundFill = new BackgroundFill(backgroundColor, null, null);
         Background background = new Background(backgroundFill);
         setBackground(background);
+
+
+        // Top half is Options/Commit Area
+        commitHistoryPane = new CommHistoryPane();
+        getItems().add(commitHistoryPane);
+        System.out.println((commitHistoryPane.getWidth()));
+        System.out.println((commitHistoryPane.getHeight()));
+//        VBox.setVgrow(commitHistoryPane, Priority.ALWAYS);
+
+        //Bottom half is Packages/Files Area
+        commitDetailsPane = new CommitDetailsPane();
+        getItems().add(commitDetailsPane);
+//        VBox.setVgrow(commitDetailsPane, Priority.ALWAYS);
     }
 }
