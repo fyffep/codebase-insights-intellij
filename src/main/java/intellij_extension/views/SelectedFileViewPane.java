3bbeeb583aec84f0dd4b4db1dc4527730da347d1
ramya <rramsam@iu.edu>
2021-11-06 14:48:17 -0400

    Initial commit for the branch

---
+++ src/main/java/intellij_extension/views/SelectedFileViewPane.java
@@ -0,0 +1,28 @@
+package intellij_extension.views;
+
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundFill;
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.Text;
+
+public class SelectedFileViewPane extends Pane {
+
+    public PackagesFilesPane() {
+        super();
+
+        Text text = new Text();
+        text.setX(40);
+        text.setY(100);
+        text.setFont(new Font(25));
+        text.setText("PackagesFilesView");
+        getChildren().add(text);
+
+        // Fill background with default
+        Color backgroundColor = Color.INDIANRED;
+        BackgroundFill backgroundFill = new BackgroundFill(backgroundColor, null, null);
+        Background background = new Background(backgroundFill);
+        setBackground(background);
+    }
+}
