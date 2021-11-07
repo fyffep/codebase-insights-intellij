5585a273d42475ebfc64ec2ff46ea92ea8831091
ramya <rramsam@iu.edu>
2021-11-06 20:47:19 -0400

    HeatMap pane and SelectedFile pane made into split pane

--- src/main/java/intellij_extension/views/SelectedFileViewPane.java
+++ src/main/java/intellij_extension/views/SelectedFileViewPane.java
@@ -1,28 +1,46 @@
 package intellij_extension.views;
 
-import javafx.scene.layout.Background;
-import javafx.scene.layout.BackgroundFill;
-import javafx.scene.layout.Pane;
+import javafx.geometry.Orientation;
+import javafx.scene.control.Label;
+import javafx.scene.control.SplitPane;
+import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 import javafx.scene.text.Text;
 
-public class SelectedFileViewPane extends Pane {
+public class SelectedFileViewPane extends AnchorPane{
 
-    public PackagesFilesPane() {
+    public SelectedFileViewPane() {
         super();
 
         Text text = new Text();
         text.setX(40);
         text.setY(100);
         text.setFont(new Font(25));
-        text.setText("PackagesFilesView");
+        text.setText("SelectedFileViewPane");
         getChildren().add(text);
 
         // Fill background with default
-        Color backgroundColor = Color.INDIANRED;
+        Color backgroundColor = Color.YELLOW;
         BackgroundFill backgroundFill = new BackgroundFill(backgroundColor, null, null);
         Background background = new Background(backgroundFill);
         setBackground(background);
+    }
+    public VBox createVBox()
+    {
+        VBox vbox=new VBox();
+        Label label=new Label("Test");
+        vbox.getChildren().add(label);
+        return vbox;
+
+    }
+
+    public void createAnchorPane()
+    {
+        SplitPane splitPane=new SplitPane();
+        VBox vbox=this.createVBox();
+        VBox.setVgrow(this, Priority.ALWAYS);
+        splitPane.getItems().add(vbox);
+        splitPane.setOrientation(Orientation.HORIZONTAL);
     }
 }
