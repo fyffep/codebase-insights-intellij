5d79f88752cac1cb35b30ff1d5476a7cf9fcc63a
ramya <rramsam@iu.edu>
2021-11-07 16:04:19 -0500

    Updated Selected File View to use View Factory class

--- src/main/java/intellij_extension/views/SelectedFileVBox.java
+++ src/main/java/intellij_extension/views/SelectedFileVBox.java
@@ -17,22 +17,22 @@ public class SelectedFileVBox extends VBox {
 
         //File Name Node
         fileName = ViewFactory.getInstance().createOrGetText(Constants.SF_FILENAME_TEXT_ID); // create a Text node
-        setFileDetailsTextProperties(fileName); // set the font
-        String formattedFieldName=formatSelectedFileFieldName("%-20s", Constants.SF_TEXT_FILENAME); //format the  Text node
+        String formattedFieldName=formatFieldName("%-20s", Constants.SF_TEXT_FILENAME); //format the  Text node
+//        setFileDetailsTextProperties(fileName); // set the font
         setSelectedFileText(fileName,Constants.SF_TEXT_FILENAME); // set the text of the Text Node
         ViewFactory.setPaneChild(this, fileName); //add the Text Node to VBox
 
         //Package Name Node
         packageName = ViewFactory.getInstance().createOrGetText(Constants.SF_PACKAGENAME_TEXT_ID);
-        setFileDetailsTextProperties(packageName);
-        formattedFieldName=formatSelectedFileFieldName("%-15s", Constants.SF_TEXT_PACKAGENAME); //format the  Text node
+        formattedFieldName=formatFieldName("%-15s", Constants.SF_TEXT_PACKAGENAME); //format the  Text node
+//        setFileDetailsTextProperties(packageName);
         setSelectedFileText(packageName,Constants.SF_TEXT_PACKAGENAME);
         ViewFactory.setPaneChild(this, packageName);
 
         //Author Node
         authors = ViewFactory.getInstance().createOrGetText(Constants.SF_AUTHOR_TEXT_ID);
-        setFileDetailsTextProperties(authors);
-        formattedFieldName=formatSelectedFileFieldName("%-22s", Constants.SF_TEXT_AUTHORS); //format the  Text node
+        formattedFieldName=formatFieldName("%-22s", Constants.SF_TEXT_AUTHORS); //format the  Text node
+//        setFileDetailsTextProperties(authors);
         setSelectedFileText(authors,Constants.SF_TEXT_AUTHORS);
         ViewFactory.setPaneChild(this, authors);
     }
@@ -53,7 +53,7 @@ public class SelectedFileVBox extends VBox {
     {
         text.setText(fieldValue);
     }
-    public String formatSelectedFileFieldName(String specifier,String fieldName)
+    public String formatFieldName(String specifier,String fieldName)
     {
         String formattedFieldName = String.format(specifier, fieldName);
         formattedFieldName = formattedFieldName + ": " ;
