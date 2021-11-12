c53dfaeb18cb91b1e634e9a5f35e363e02b6c454
Abhishek Dharmendra Tiwari <abtiwa@iu.edu>
2021-11-12 03:55:01 -0500

    Changes in the redesign model to include the business logic for heatmap from JGit
    Adjust the CommitCountCalculator (JGit) to account for the new model design

--- src/main/java/intellij_extension/models/redesign/CodebaseV2.java
+++ src/main/java/intellij_extension/models/redesign/CodebaseV2.java
@@ -2,16 +2,24 @@ package intellij_extension.models.redesign;
 
 import com.google.common.collect.HashBasedTable;
 import intellij_extension.models.Commit;
+import intellij_extension.utility.commithistory.CommitCountCalculator;
+import javafx.util.Pair;
+import org.eclipse.jgit.lib.ObjectLoader;
+import org.eclipse.jgit.revwalk.RevCommit;
+import org.eclipse.jgit.treewalk.TreeWalk;
 
-import java.util.ArrayList;
-import java.util.HashSet;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.LinkedHashMap;
+import java.util.LinkedHashSet;
 
 public class CodebaseV2 {
 
     private String activeBranch;
-    private ArrayList<String> branchNameList;
-    private HashSet<Commit> activeCommits;
-    private HashSet<FileObjectV2> activeFileObjects;
+    private LinkedHashSet<String> branchNameList;
+    private LinkedHashSet<Commit> activeCommits;
+    private LinkedHashSet<FileObjectV2> activeFileObjects;
 
     // TODO
     // I think this is the exact sort of DataStructure we want for this.
@@ -25,49 +33,79 @@ public class CodebaseV2 {
 
     public CodebaseV2() {
         activeBranch = "master";
-        branchNameList = new ArrayList<>();
-        activeCommits = new HashSet<>();
-        activeFileObjects = new HashSet<>();
+        branchNameList = new LinkedHashSet<>();
+        activeCommits = new LinkedHashSet<>();
+        activeFileObjects = new LinkedHashSet<>();
         commitToFileAssociation = HashBasedTable.create();
     }
 
     // TODO
     // Is this the JGit object?
-    public void buildBranchData(String branch) {
-        // for (RevCommit rCommit : CommitCountCalculator.getCommitsByBranch(branch)) {
-        // CommitV2 newCommit = new CommitV2(rCommit);
-        // TODO - This does not exist - Might be looping over diff entry?
-        // for (File file: newCommit.files) {
-        // If File already exists in commitToFileAssocation
-        // CalculateHeat with history taken into account
-        // Else
-        // CalculateHeat without history
-        // BuildFileObject
-        // FileObjectV2 newFile = new FileObjectV2();
-        // activeFileOjbects.add(newFile);
-        // commitToFileAssociation.put(newFile.getFilename(), newCommit.getHash(), 1);
-        // }
+    public void buildBranchData(String branch) throws IOException {
+        Pair<Iterable<RevCommit>, TreeWalk> treeWalkCommitsPair = CommitCountCalculator.getCommitsAndTreeByBranch(branch);
+        for (RevCommit rCommit : treeWalkCommitsPair.getKey()) {
+            TreeWalk treeWalk = treeWalkCommitsPair.getValue();
+            treeWalk.addTree(rCommit.getTree());
+            CommitV2 newCommit = new CommitV2(rCommit);
 
-        // Fill out table with 0s for files that are not in commit.
-        // Foreach file NOT in new Commit
-        // commitToFileAssociation.put(newFile.getFilename(), newCommit.getHash(), 0);
-        // }
+            while (treeWalk.next()) {
+                //This can be used if there's a need for the File object to be stored
+                //treeWalk.getObjectId(0).copyTo(System.out);
+                String fileName = treeWalk.getNameString();
+                String filePath = treeWalk.getPathString();
+                ObjectLoader loader = CommitCountCalculator.getObjectLoader(treeWalk.getObjectId(0));
+                FileObjectV2 fileObject = new FileObjectV2(Paths.get(filePath), fileName, new LinkedHashMap<>());
+
+                FileObjectV2 existingFileObject = activeFileObjects.stream()
+                        .filter(fileObjectV2 -> fileObject.equals(fileObjectV2)).findAny().orElse(fileObject);
+
+                LinkedHashMap<String, HeatObject> commitHashMap = existingFileObject.getCommitHashToHeatObjectMap();
+                HeatObject heatObject;
+                if (commitHashMap.isEmpty()) {
+                    heatObject = new HeatObject(1, fileName, getLineCount(filePath),
+                            loader.getSize(), 1);
+                } else {
+                    heatObject = computeHeatObjectFromHistory(commitHashMap, existingFileObject,
+                            fileName, filePath, loader);
+                }
+                existingFileObject.setHeatForCommit(newCommit.getHash(), heatObject);
+                activeFileObjects.add(existingFileObject);
+
+
+                /** I gave the 2D array/HashBasedTable idea another thought and came to the conclusion that either way is
+                 * a very memory-intensive approach.
+                 * If we go via the 2D array approach, iterating through a huge one for instance 1000 indexes of primitives
+                 * can cause a bottleneck. On the other way round, if we discard are to get away from the idea of 2D array,
+                 * there is no need to maintain 0 as a value for broken links (i.e. files that were never a part of a commit)
+                 * It would be too much to hold for a drop of blood.
+                 * This leads us to see HashMaps v/s HashBasedTable v/s Set of fileNames in every CommitV2 object
+                 * In this comparison, HashMaps would win for sizes ranging from 1 to 10000 and it is only beyond that value
+                 * is when using HashBasedTable would actually make sense. If we are no longer maintaining the broken links (0)
+                 * as a value, a simple HashMap would have done the job for us, However, it's better to just have a simple
+                 * Set of fileName string per CommitV2 object.
+                 * If the above justification sounds OK, we can remove the HashBasedTable init.
+                **/
+                newCommit.addFileToSet(fileName);
+            }
+        }
     }
 
-    // Just made the definitions to remove errors
-    // These can change.
-    public void heatMapObjectSelected(String id) {
+    private HeatObject computeHeatObjectFromHistory(LinkedHashMap<String, HeatObject> commitHashMap,
+                                                    FileObjectV2 existingFileObject, String fileName, String filePath, ObjectLoader loader) {
+        HeatObject previousHeat = commitHashMap.get(existingFileObject.getLatestCommit());
+        float latestHeatLevel = previousHeat.getHeatLevel();
+        int prevNumOfCommits = previousHeat.getNumberOfCommits();
+        return new HeatObject(++latestHeatLevel, fileName, getLineCount(filePath), loader.getSize(), ++prevNumOfCommits);
     }
 
-    public void branchSelected(String branchName) {
-    }
-
-    public void commitSelected(String commitHash) {
-    }
-
-    public void changeHeatMapToCommit(String commitHash) {
-    }
-
-    public void openFile(String filename) {
+    // TODO: Need to find a way to get the number of lines through commit history
+    //  The main question here is, is it required/worth to maintain the history of file per commit?
+    private long getLineCount(String filePath) {
+        try {
+            // This technique would return the lines of the file in the latest version of the repository
+            return Files.lines(Paths.get(filePath)).count();
+        } catch (IOException e) {
+            return 0;
+        }
     }
 }
