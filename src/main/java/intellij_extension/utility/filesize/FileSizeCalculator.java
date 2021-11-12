adbe2e934d50f79201b4a5708d5f93e9db027f3c
Behar, Ethan T <ebehar@iu.edu>
2021-11-12 09:09:35 -0500

    re-arranging methods computeHeatObjectFromHistory moved to heatCalculationUtility -- getLineCount moved to FileSizeCalculator

--- src/main/java/intellij_extension/utility/filesize/FileSizeCalculator.java
+++ src/main/java/intellij_extension/utility/filesize/FileSizeCalculator.java
@@ -6,6 +6,9 @@ import intellij_extension.models.FileObject;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 
 public class FileSizeCalculator
@@ -40,8 +43,24 @@ public class FileSizeCalculator
             // TODO what does this mean for a plugin project??
             System.exit(0);
         }
+
         //Assign the line count
         fileObject.setLineCount(lineCount);
+    }
+
+    /**
+     * Gets the line count of a file at the given filePath
+     * @param filePath
+     */
+    // TODO: Need to find a way to get the number of lines through commit history
+    //  The main question here is, is it required/worth to maintain the history of file per commit?
+    public static long getLineCount(String filePath) {
+        try {
+            // This technique would return the lines of the file in the latest version of the repository
+            return Files.lines(Paths.get(filePath)).count();
+        } catch (IOException e) {
+            return 0;
+        }
     }
 
     /**
